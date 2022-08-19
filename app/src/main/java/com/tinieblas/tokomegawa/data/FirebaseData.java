ce673b371cd5667f5be8e3ccf34fed0e92330437
SrDeLasTinieblas <xngelo_03.h@hotmail.com>
2022-08-18 23:21:40 -0500

    firebase

---
+++ app/src/main/java/com/tinieblas/tokomegawa/data/FirebaseData.java
@@ -0,0 +1,55 @@
+package com.tinieblas.tokomegawa.data;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+public class FirebaseData {
+    private String idUser;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    String name;
+
+    // Creamos la funcion para guardar los datos
+    public void getDataUser(Context context, EventListener<DocumentSnapshot> listener) {
+        final String[] apellidos = new String[1];
+        try {
+            // Llamamos a la funcion para definir firebase
+            //definingFirebase();
+            // Creamos una nueva collecion en firebase de nombre "usuario"
+            DocumentReference documentReference = firebaseFirestore.collection("Usuarios").document(idUser);
+            documentReference.addSnapshotListener(listener);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        //return apellidos[0];
+    }
+    public void uploadDataFireBase(Context context) {
+        FirebaseData firebaseData = new FirebaseData();
+        firebaseData.getDataUser(context, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(DocumentSnapshot value, FirebaseFirestoreException error) {
+                firebaseFirestore = FirebaseFirestore.getInstance();
+                // Recuperamos nombre
+                name = value.getString("nombres");
+
+                Toast.makeText(context, name+"ddddd", Toast.LENGTH_SHORT).show();
+                //sweetAlertDialog.sweetAlertSuccessRegistro(context, name);
+
+            }
+        });
+        //return name;
+    }
+
+
+
+}
+

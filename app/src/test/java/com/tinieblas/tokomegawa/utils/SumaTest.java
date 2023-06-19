835b97a275b7e058621333d669033c7fd584e590
Tinieblas <xngelo_03.h@hotmail.com>
2023-06-18 20:16:30 -0500

    haciendo testing :p

--- app/src/test/java/com/tinieblas/tokomegawa/utils/SumaTest.java
+++ app/src/test/java/com/tinieblas/tokomegawa/utils/SumaTest.java
@@ -1,6 +1,8 @@
 package com.tinieblas.tokomegawa.utils;
 
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,10 +11,14 @@ public class SumaTest {
     @Test
     public void sumando() {
         System.out.println("Sumando..");
-
+        assertEquals(5, 2 + 2);
         //Assert.assertEquals(5, 6);
     }
-
+    public void addition_isCorrect() {
+        //System.out.println("adition");
+        //Log.d("TAG", "Este es un mensaje de prueba");
+        assertEquals(5, 2 + 2);
+    }
 }
 
 
